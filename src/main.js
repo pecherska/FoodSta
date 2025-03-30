e0b2939603ea04d9e7fc2f9dd7cc24e28dffe4ee
Nina Pecherska <npecherska@gmail.com>
2025-03-30 20:04:48 +0300

    final

--- src/main.js
+++ src/main.js
@@ -1,2 +1,15 @@
 import './js/header';
 import './js/hero';
+let map;
+// initMap is now async
+async function initMap() {
+  // Request libraries when needed, not in the script tag.
+  const { Map } = await google.maps.importLibrary('maps');
+  // Short namespaces can be used.
+  map = new Map(document.getElementById('map'), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+initMap();
